0646c0f401ce37dbf69e38457dd5d7252030ae2d
MaVirgil <magnusvirgil@hotmail.com>
2025-09-15 10:30:21 +0200

    change number of messages created

--- src/main/java/com/example/simplemessageapp/repository/MessageRepository.java
+++ src/main/java/com/example/simplemessageapp/repository/MessageRepository.java
@@ -17,7 +17,7 @@ public class MessageRepository {
     }
 
     private void populateMessages() {
-        int initialNumMessages = 5;
+        int initialNumMessages = 3;
         while (latestId <= initialNumMessages - 1) {
             messages.add(new Message(getNextId(), "Dette er den " + latestId + ". besked!"));
         }
